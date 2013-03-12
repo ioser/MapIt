6a4aec116d69094e615e98632b2712e6a8b31977
Richard Millet <milletdesigns@me.com>
2013-03-11 22:42:03 -0700

    Initial Commit

---
+++ MapIt/SCSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCSAppDelegate.h
+//  MapIt
+//
+//  Created by Richard E Millet on 3/11/13.
+//  Copyright (c) 2013 Richard Millet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MapIt/SCSViewController.h
@@ -0,0 +1,13 @@
+//
+//  SCSViewController.h
+//  MapIt
+//
+//  Created by Richard E Millet on 3/11/13.
+//  Copyright (c) 2013 Richard Millet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCSViewController : UIViewController
+
+@end
