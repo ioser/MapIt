e16e81fb1e411b420e638c442f60c42eb9e1861b
Richard Millet <milletdesigns@me.com>
2013-03-14 22:24:09 -0700

    First commit.

--- MapIt/SCSViewController.m
+++ MapIt/SCSViewController.m
@@ -5,10 +5,12 @@
 //  Created by Richard E Millet on 3/11/13.
 //  Copyright (c) 2013 Richard Millet. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 #import "SCSViewController.h"
 
-@interface SCSViewController ()
+@interface SCSViewController ()<MKMapViewDelegate>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
 
@@ -18,6 +20,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//	self.mapView.showsUserLocation = YES;
+//	self.mapView.mapType = MKMapTypeHybrid;
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +29,11 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - MKMapViewDelegate methods
+
+//-  (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation {
+//	self.mapView.region = MKCoordinateRegionMakeWithDistance(self.mapView.userLocation.coordinate, 500, 500);
+//}
 
 @end
